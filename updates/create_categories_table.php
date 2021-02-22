02c38f214481832c3430eabdc56f3476d015ce2f
Lexetam <giovannybm@gmail.com>
2021-02-22 10:57:41 -0500

    Multindustrial Support

--- updates/create_categories_table.php
+++ updates/create_categories_table.php
@@ -1,14 +1,13 @@
 <?php namespace Lexetam\Catalog\Updates;
 
 use Schema;
-use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 
 class CreateCategoriesTable extends Migration
 {
     public function up()
     {
-        Schema::create('lexetam_catalog_categories', function(Blueprint $table) {
+        Schema::create('lexetam_catalog_categories', function($table) {
             $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('category_name',200);
--- updates/create_products_table.php
+++ updates/create_products_table.php
@@ -1,14 +1,13 @@
 <?php namespace Lexetam\Catalog\Updates;
 
 use Schema;
-use October\Rain\Database\Schema\Blueprint;
 use October\Rain\Database\Updates\Migration;
 
 class CreateProductsTable extends Migration
 {
   public function up()
   {
-    Schema::create('lexetam_catalog_products', function(Blueprint $table) {
+    Schema::create('lexetam_catalog_products', function($table) {
       $table->engine = 'InnoDB';
       $table->increments('id');
       $table->string('product_name', 300);
